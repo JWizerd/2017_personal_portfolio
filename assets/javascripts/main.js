abd5af603979683825c35b0ec112b09420f39dc1
JWizerd <jwodke@outlook.com>
2017-02-14 11:18:26 -0700

    jquery localled integrated and IIFE set up for personal code encapsulation

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,1 +1,4 @@
-
+(function(global, $){
+  // Immediately Invoked Fucntion Expresion invoked to create a new execution context therefore creating encapsulation any variables from the global object
+  var dude = "hey";
+}(window, jQuery));
