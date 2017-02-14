f04bee9edfd15c6450e51e85b980c98592948c42
JWizerd <jwodke@outlook.com>
2017-02-14 14:51:55 -0700

    javascript for hover links configured

--- _site/assets/javascripts/main.js
+++ _site/assets/javascripts/main.js
@@ -1,5 +1,29 @@
 (function($){
   // Immediately Invoked Fucntion Expresion invoked to create a new execution context therefore creating encapsulation any variables from the global object
 
-  $(document).ready(function() {});
+  $navLinks = $('.navigation ul li a');
+  $icon = $('.navigation ul li');
+
+  function addLinkTitlesToNavLinks() {
+    $($navLinks).each(function() {
+      $title = $(this).attr('title');
+      $(this).find('span').html($title);
+    });
+  }
+
+  function linkTitleOnHover() {
+    $navLinks.hover(function() {
+      $this = $(this);
+      $this.find('i').hide();
+      $this.find('span').show();
+    }, function () {
+      $this.find('i').show();
+      $this.find('span').hide();
+    });
+  }
+
+  $(document).ready(function() {
+    addLinkTitlesToNavLinks();
+    linkTitleOnHover();
+  });
 }(jQuery));
