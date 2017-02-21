2b626b98ff71be84082b93b6ffa8d472ad359fdc
JWizerd <jwodke@outlook.com>
2017-02-21 11:55:25 -0700

    formspree ajax configured

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -28,6 +28,38 @@
   /* add animation before load for smoothest effect */
   addAnimation('.subpage-title', 'bounceInLeft');
 
+
+  /* formspree */
+  function formSpreeAjax() {
+  var $contactForm = $('.contact-form');
+  var $btn = $('.btn-submit');
+  $contactForm.submit(function(e) {
+  	e.preventDefault();
+  	$.ajax({
+  		url: '//formspree.io/jeremiah@impactstud.io',
+  		method: 'POST',
+  		data: $(this).serialize(),
+  		dataType: 'json',
+  		beforeSend: function() {
+        $btn.addClass('alert--loading');
+  			$btn.prop('value', 'Sending Message...');
+  		},
+  		success: function(data) {
+  			$btn.find('.alert--loading').hide();
+  			$btn.prop('value', 'Message Sent!');
+        $btn.addClass('my-animate bounce sun');
+        window.setTimeout(function() {
+          $btn.removeClass('my-animate bounce bg-sun');
+        }, 3000)
+  		},
+  		error: function(err) {
+  			$contactForm.find('.alert--loading').hide();
+  			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+  		}
+  	});
+  });
+}
+
   $(document).ready(function() {
 
     /* animations */
@@ -39,6 +71,8 @@
     loadViewOnClick('.contact', 'contact.html');
     loadViewOnClick('.home', 'home.html');
     loadViewOnClick('.work', 'work.html');
+
+    formSpreeAjax();
 
     /* bug fix for modal window and animation.css */
     window.setTimeout(function() {
