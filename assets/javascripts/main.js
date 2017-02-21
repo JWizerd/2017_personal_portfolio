781b17584bbd06953f8ee6637a52c932c669c818
JWizerd <jwodke@outlook.com>
2017-02-21 15:30:44 -0700

    im bummed but the website had to reverted back to multipage website not single page application

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -12,14 +12,15 @@
     );
   }
 
-  function loadViewOnClick(elm, view, location) {
-    var location = '.view div' || location;
-    $(elm).on("click", function(e){
-      e.preventDefault();
-      $(location).load(view);
-      $.getScript('assets/javascripts/main.js');
-    });
-  }
+  // COME BACK TO LATER WITH FRESH MIND
+  // function loadViewOnClick(elm, view, location) {
+  //   var location = '.view div' || location;
+  //   $(elm).on("click", function(e){
+  //     e.preventDefault();
+  //     $(location).load(view);
+  //     $.getScript('assets/javascripts/main.js');
+  //   });
+  // }
 
   function addAnimation(elm, animation) {
     $(elm).addClass('my-animate ' + animation);
@@ -28,6 +29,38 @@
   /* add animation before load for smoothest effect */
   addAnimation('.subpage-title', 'bounceInLeft');
 
+
+  /* formspree */
+  function formSpreeAjax() {
+  var $contactForm = $('.contact-form');
+  var $btn = $('.btn-submit');
+  $contactForm.submit(function(e) {
+  	e.preventDefault();
+  	$.ajax({
+  		url: '//formspree.io/jeremiah@impactstud.io',
+  		method: 'POST',
+  		data: $(this).serialize(),
+  		dataType: 'json',
+  		beforeSend: function() {
+        $btn.addClass('alert--loading');
+  			$btn.prop('value', 'Sending Message...');
+  		},
+  		success: function(data) {
+  			$btn.find('.alert--loading').hide();
+  			$btn.prop('value', 'Message Sent!');
+        $btn.addClass('my-animate bounce sun');
+        window.setTimeout(function() {
+          $btn.removeClass('my-animate bounce bg-sun');
+        }, 3000)
+  		},
+  		error: function(err) {
+  			$contactForm.find('.alert--loading').hide();
+  			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+  		}
+  	});
+  });
+}
+
   $(document).ready(function() {
 
     /* animations */
@@ -35,10 +68,10 @@
 
 
     /* single page navigation */
-    loadViewOnClick('.about', 'about.html');
-    loadViewOnClick('.contact', 'contact.html');
-    loadViewOnClick('.home', 'home.html');
-    loadViewOnClick('.work', 'work.html');
+    // loadViewOnClick('.about', 'about.html');
+    // loadViewOnClick('.contact', 'contact.html');
+    // loadViewOnClick('.home', 'home.html');
+    // loadViewOnClick('.work', 'work.html');
 
     /* bug fix for modal window and animation.css */
     window.setTimeout(function() {
