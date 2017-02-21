a1492ec9acb267e5d29d00db1db85a9a54fc928e
JWizerd <jwodke@outlook.com>
2017-02-21 11:29:53 -0700

    navigation issues fixed

--- _site/assets/javascripts/main.js
+++ _site/assets/javascripts/main.js
@@ -1,27 +1,6 @@
 (function($){
   // Immediately Invoked Fucntion Expresion invoked to create a new execution context therefore creating encapsulation any variables from the global object
 
-  $navLinks = $('.navigation ul li a');
-  $icon = $('.navigation ul li');
-
-  function addLinkTitlesToNavLinks() {
-    $($navLinks).each(function() {
-      $title = $(this).attr('title');
-      $(this).find('span').html($title);
-    });
-  }
-
-  function linkTitleOnHover() {
-    $navLinks.hover(function() {
-      $this = $(this);
-      $this.find('i').hide();
-      $this.find('span').show();
-    }, function () {
-      $this.find('i').show();
-      $this.find('span').hide();
-    });
-  }
-
   function animateOnHover(elm, animation) {
     $(elm).hover(
       function(){
@@ -52,8 +31,6 @@
   $(document).ready(function() {
 
     /* animations */
-    addLinkTitlesToNavLinks();
-    linkTitleOnHover();
     animateOnHover('.client-logo', 'tada');
 
 
