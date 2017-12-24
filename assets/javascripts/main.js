b34785ea0cfe37fbccc77d6b13e9b7e03ee283c2
JWizerd <jwodke@outlook.com>
2017-12-24 08:54:50 -0700

    updated gh endpoint for newest repos first

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -92,8 +92,10 @@
       if ($('.drawer').hasClass('bounceInLeft')) {
         $('.drawer').removeClass('bounceInLeft')
         $('.drawer').addClass('bounceOutLeft')
-      } else if($('.drawer').hasClass('bounceOutLeft')) {
+      } else if ($('.drawer').hasClass('bounceOutLeft')) {
         $('.drawer').removeClass('bounceOutLeft')
+        $('.drawer').addClass('bounceInLeft')
+      } else {
         $('.drawer').addClass('bounceInLeft')
       }
     })
